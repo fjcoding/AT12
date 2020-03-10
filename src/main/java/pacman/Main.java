2c37aa0dd80f798e13c14ee3b8ee3f14ec749366
Jose Guevara Romero <joseguevara@c305-05.fundacion-jala.local>
2020-03-10 18:26:57 -0400

    Fixed compilation error of Main.java

--- src/main/java/pacman/Main.java
+++ src/main/java/pacman/Main.java
@@ -14,19 +14,17 @@ class Main extends Frame{
         this.setSize(200,200);
 		this.setVisible(true);
 
-        w = new ArrayList <wall>();     //create arraylist for outer wall
+       // w = new ArrayList <wall>();     //create arraylist for outer wall
         //fill outer wall with -
         //Initialize object wall
-        for( int i=0; i<400;i+=10){
-            w.add(new wall(1,i,true,"-"));
-        }
+        
     }
 
     public static void main(String[] args){
-        p = new pacman(8, 8, true);
-        f = new food(2, 1, true);
-        g = new ghost (3, 1, true);
-        w = new wall (4, 1, true);
+        //p = new pacman(8, 8, true);
+        //f = new food(2, 1, true);
+        //g = new ghost (3, 1, true);
+        //w = new wall (4, 1, true);
         
         Main m = new Main(); 
     }
