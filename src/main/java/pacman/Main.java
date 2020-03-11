cdf0d743ea1ac53bdbb8c3ebb81ffed892d5d2df
Angela Martinez Aguilar <angelamartinez@c305-07.fundacion-jala.local>
2020-03-11 17:48:29 -0400

    Add functions move up down left right of class ghost

--- src/main/java/pacman/Main.java
+++ src/main/java/pacman/Main.java
@@ -1,33 +1,53 @@
 import java.awt.*;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.*;
+
+
 class Main extends Frame{
 
     //attrib
-    private pacman p;
-    private food f;
-    private ghost g;
-    private ArrayList <wall> w; 
+    //private pacman p;
+    //private food f;
+    //private ghost g;
+    //private ArrayList <wall> w; 
 
     public Main(){
         this.setLayout(null);
-        this.setSize(200,200);
+        this.setSize(900,900);
 		this.setVisible(true);
-
+        //JPanel panel = new JPanel();
+        //panel.setPreferredSize(new Dimension(900,900));
+        ///panel.setBackground(Color.BLACK);
+        //
+        //JLabel label = new JLabel(ghost);
+        //panel.add(label);
        // w = new ArrayList <wall>();     //create arraylist for outer wall
         //fill outer wall with -
         //Initialize object wall
         
     }
-
+    public void paint(Graphics g,int x, int y){
+        //g.setColor(Color.RED);
+        ImageIcon ghost = new ImageIcon("images/ghost.jpg");
+        Image ghostImg = ghost.getImage();
+        g.drawImage(ghostImg,0,0,50,50,null);
+        
+    }
     public static void main(String[] args){
         //p = new pacman(8, 8, true);
         //f = new food(2, 1, true);
         //g = new ghost (3, 1, true);
         //w = new wall (4, 1, true);
-        
+        ghost ghost1 = new ghost(1,1,true);
+        Thread ghostThread = new Thread(ghost1);
+        ghostThread.start();
+        //Graphics g = new Graphics();
+        paint(ghost1.getX(), ghost1.getY());
+         
         Main m = new Main(); 
+        
     }
-    
 
 }
--- src/main/java/pacman/ghost.java
+++ src/main/java/pacman/ghost.java
@@ -1,7 +1,55 @@
-class ghost extends position {
-    private boolean eatable;
+class ghost extends position implements Runnable{
+
+	private boolean eatable;
 	public ghost (int x, int y,boolean exist){
-		super(x,y,exist);
-        this.eatable = false;
+	 super(x,y,exist);
+			this.eatable = false;
 	}
-}
+	
+	public boolean getExist(){
+	 return exist;
+	}
+	public void setX(int x){
+	 super.x = x;
+	}
+	public void setY(int y){
+	 super.y = y;
+	}
+	public boolean setExist(boolean exist){
+	 super.exist = exist;
+	 return true;
+	}
+	public void moveUp(){
+	 super.y--;
+	}
+	public void moveDown(){
+	 super.y++;
+	}
+	public void moveLeft(){
+	 super.x--;
+	}
+	public void moveRight(){
+	 super.x++;
+	}
+	
+	public boolean isPosibleMoveDown(){
+		return true;
+	}
+	public void searchRoute(){
+		if(isPosibleMoveDown()){
+			moveDown();
+		}
+		else{
+			moveUp();
+		}
+	}
+	@Override
+	public void run(){
+		while(exist){
+			searchRoute();
+		}
+	}
+	public void stopRun(){
+			exist = false;
+		}
+	}
