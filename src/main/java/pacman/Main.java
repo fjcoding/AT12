72cd592a8a65319ee984a3dc819891ae896dbe66
Julia Escalante Cruz <juliaescalante@c305-09.fundacion-jala.local>
2020-03-12 19:12:43 -0400

    fixed code review errors and added comments to methods

--- src/main/java/pacman/Main.java
+++ src/main/java/pacman/Main.java
@@ -1,45 +1,46 @@
 import javax.swing.JFrame;
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 class Main extends JFrame implements KeyListener{
 
-    PacmanDraw pacmanDraw;
+    PacmanDraw pacmanDraw ;
  
     public Main(){
         this.pacmanDraw = new PacmanDraw();
         addKeyListener(this);
+        this.setSize(900, 900);
         this.setVisible(true);       
     }
 
     @Override
-    public void keyPressed(KeyEvent event) {
+    public void keyPressed(KeyEvent event){
     }
 
     @Override
-    public void keyReleased(KeyEvent event) {
-        if(event.getKeyCode()== KeyEvent.VK_RIGHT)
+    public void keyReleased(KeyEvent event){
+        if(event.getKeyCode() == KeyEvent.VK_RIGHT)
             pacmanDraw.moveRight();
-        else if(event.getKeyCode()== KeyEvent.VK_LEFT)
+        else if(event.getKeyCode() == KeyEvent.VK_LEFT)
             pacmanDraw.moveLeft();
-        else if(event.getKeyCode()== KeyEvent.VK_DOWN)
+        else if(event.getKeyCode() == KeyEvent.VK_DOWN)
             pacmanDraw.moveDown();
-        else if(event.getKeyCode()== KeyEvent.VK_UP)
+        else if(event.getKeyCode() == KeyEvent.VK_UP)
             pacmanDraw.moveUp();
     }
 
     @Override
-    public void keyTyped(KeyEvent event) {
+    public void keyTyped(KeyEvent event){
     }
 
     public static void main(String[] args){       
+        
         Main frame = new Main();
-        frame.setResizable(false);
-        frame.setSize(900, 900);
+        frame.setMinimumSize(new Dimension(900, 900));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(frame.pacmanDraw);
         frame.pack();
-        frame.setVisible(true);
-        
+        frame.setVisible(true);       
     }  
 }
--- src/main/java/pacman/PacmanDraw.java
+++ src/main/java/pacman/PacmanDraw.java
@@ -8,29 +8,34 @@ public class PacmanDraw extends JComponent {
     public int x = 50;
     public int y = 50;
     
-    public void paintComponent(Graphics g) {
+    //Method to draw pacman in the frame
+    public void paintComponent(Graphics g){
         ImageIcon pacman = new ImageIcon("pacmanIcon.png");
         Image pacmanImg = pacman.getImage();
         g.drawImage(pacmanImg,x,y,30,30,null);
     }
 
-    public void moveRight() {
-        x = x + 1;
+    //Method to move pacman to right 10 pixels
+    public void moveRight(){
+        x = x + 10;
         repaint();
     }
 
-    public void moveLeft() {
-        x = x - 1;
+    //Method to move pacman to left 10 pixels
+    public void moveLeft(){
+        x = x - 10;
         repaint();
     }
 
-    public void moveDown() {
-        y = y + 1;
+    //Method to move pacman to down 10 pixels
+    public void moveDown(){
+        y = y + 10;
         repaint();
     }
 
-    public void moveUp() {
-        y = y - 1;
+    //Method to move pacman to up 10 pixels
+    public void moveUp(){
+        y = y - 10;
         repaint();
     }
 }
