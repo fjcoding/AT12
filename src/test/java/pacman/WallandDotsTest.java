46d98ee7d991cce3fc654b99d0f8a27cb1ca6b7b
Jhordan Soto Mercado <jhordansoto@c305-06.fundacion-jala.local>
2020-03-12 17:48:21 -0400

    changes made after code review

--- src/test/java/pacman/WallandDotsTest.java
+++ src/test/java/pacman/WallandDotsTest.java
@@ -1,14 +1,12 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class WallandDotsTest {
+public class WallAndDotsTest {
 	@Test
 	public void testExist(){
         int x=1;
         int y=1;
 		wall wall1_1 = new wall(x,y, true);
-        //wall1_1.positionObj(1,2);
         assertEquals(1,wall1_1.getY());
         assertEquals(1,wall1_1.getX());
     }
