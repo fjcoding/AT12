5dabfda32bface4fca37ae1ab046a16eec48ba80
juls <julia.escalante@fundacion-jala.org>
2020-03-31 10:57:50 -0400

    pacman does not walk on the walls

--- src/main/java/pacman/Pacman.java
+++ src/main/java/pacman/Pacman.java
@@ -1,26 +1,64 @@
-class Pacman extends Position{
+import java.util.ArrayList;
 
-    //private int x, y;
-    // private List<Wall> wall;g
+class Pacman extends Position {
+
+    protected ArrayList <Position> positions;
+
     public Pacman (int x, int y, boolean exist){
         super(x, y, exist);
+        positions = new ArrayList<Position>();
+        positions.add(new Wall(0,0, true));
+        positions.add(new Wall(0,30, true));
+        positions.add(new Wall(0,60, true));
+        positions.add(new Wall(0,90, true));
+        positions.add(new Wall(0,120, true));
+
+        positions.add(new Wall(30,0, true));
+        positions.add(new Wall(60,0, true));
+        positions.add(new Wall(90,0, true));
+        positions.add(new Wall(120,0, true));
     }
     
     //functions those add and substract pacman's positions	
     public int down(){
-        return super.y += 30;
+        if(existDistintPacman(x, y+30)){
+            return super.y = y;
+        }else{
+            return super.y += 30;
+        }
     }
     
     public int right(){
-        return super.x += 30;
+        if(existDistintPacman(x+30, y)){
+            return super.x = x;
+        }else{
+            return super.x += 30;
+        }
     }
     
     public int left(){
-        return super.x -= 30;
+        if(existDistintPacman(x-30, y)){
+            return super.x = x;
+        }else{
+            return super.x -= 30;
+        }
     }
     
     public int up(){
-        return super.y -= 30;
+        if(existDistintPacman(x, y-30)){
+            return super.y = y;
+        }else{
+            return super.y -= 30;
+        }
+    }
+
+    public boolean existDistintPacman(int xx, int yy){
+        boolean exist=false;
+        for(Position i: positions){
+            if(i.getX() ==xx && i.getY()==yy)
+                exist= true;
+        }
+        return exist;
     }
 
     public boolean isEatable(Dot dotsPosition){
--- src/main/java/pacman/PacmanDraw.java
+++ src/main/java/pacman/PacmanDraw.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
@@ -12,32 +13,56 @@ public class PacmanDraw extends JComponent {
     
     //Method to draw pacman in the frame
     public void paintComponent(Graphics g){
+        drawWall(g);
         ImageIcon pacmanIcon = new ImageIcon("pacmanIcon.png");
         Image pacmanImg = pacmanIcon.getImage();
         g.drawImage(pacmanImg,x,y,30,30,null);
     }
+    public void drawWall(Graphics g){
+        g.setColor(Color.BLUE);
+        g.drawRect(0, 0, 30,30);//izquierda
+        g.drawRect(0, 30, 30,30);
+        g.drawRect(0, 60, 30,30);
+        g.drawRect(0, 90, 30,30);
+        g.drawRect(0, 120, 30,30);
 
+        g.drawRect(30, 0, 30, 30);//arriba
+        g.drawRect(60, 0, 30, 30);
+        g.drawRect(90, 0, 30, 30);
+        g.drawRect(120, 0, 30, 30);
+        g.drawRect(870, 870, 30, 30);
+
+        g.drawRect(870, 0, 30, 30); // derecha
+        g.drawRect(870, 30, 30, 30);
+
+        g.drawRect(0, 870, 30, 30); // abajo
+        g.drawRect(30, 870, 30, 30);
+    }
     //Method to move pacman to right 10 pixels
     public void moveRight(){
         x = pacman.right();
+        y = y;
         repaint();
     }
 
     //Method to move pacman to left 10 pixels
     public void moveLeft(){
         x = pacman.left();
+        y= y;
         repaint();
     }
 
     //Method to move pacman to down 10 pixels
     public void moveDown(){
         y = pacman.down();
+        x= x;
         repaint();
     }
 
     //Method to move pacman to up 10 pixels
     public void moveUp(){
         y = pacman.up();
+        x = x;
         repaint();
     }
 }
