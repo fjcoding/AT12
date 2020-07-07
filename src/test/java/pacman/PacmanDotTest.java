63626054427d24eba32d18d35364ad328bf33282
Samuel Loza Ramirez <starsaminf@gmail.com>
2020-07-07 17:54:13 -0400

    Update PacmanDotTest.java and removed Ghost test

--- src/test/java/pacman/PacmanDotTest.java
+++ src/test/java/pacman/PacmanDotTest.java
@@ -4,39 +4,23 @@ import org.junit.Test;
 public class PacmanDotTest{
 	
 	@Test
-	public void testFood(){
+	public void testIsFood(){
 		Pacman p = new Pacman(5, 5, true);
-		Dot   f = new Dot(5,5,true);		
-		assertEquals(true,p.isEatable(f));	
+		Dot f = new Dot(5, 5, true);		
+		assertEquals(true, p.isEatable(f));	
 	}
 
 	@Test
 	public void testFoodNoExists(){
 		Pacman p = new Pacman(5, 5, true);
-		Dot   f = new Dot(5,5,false);		
-		assertEquals(false,p.isEatable(f));		
+		Dot f = new Dot(5, 5, false);		
+		assertEquals(false, p.isEatable(f));		
 	}
 	
 	@Test
 	public void testFoodNoEatable(){
 		Pacman p = new Pacman(5, 5, true);
-		Dot   f = new Dot(5, 4, true);	
-		assertEquals(false,p.isEatable(f));		
+		Dot f = new Dot(5, 4, true);	
+		assertEquals(false, p.isEatable(f));		
 	}
-	
-	@Test
-	public void testFoodGhostNoEatable(){
-		Pacman p = new Pacman(5, 5, true);
-		Ghost   g = new Ghost(5, 5, true);	
-		assertEquals(false,p.isEatable(g));	
-	}
-
-	@Test
-	public void testFoodGhostEatable(){
-		Pacman p = new Pacman(5, 5, true);
-		Ghost   g = new Ghost(5, 5, true);
-		g.changeEatable();		
-		assertEquals(true,p.isEatable(g));
-	}
-    
 }
