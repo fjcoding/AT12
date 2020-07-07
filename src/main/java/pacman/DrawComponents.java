d3e933461547f348504d5e632bd19a50f9ac8171
roht0515 <rodrigo051599@gmail.com>
2020-07-07 19:00:52 -0400

    updating names of functions

--- src/main/java/pacman/DrawComponents.java
+++ src/main/java/pacman/DrawComponents.java
@@ -12,19 +12,19 @@ public class DrawComponents extends JComponent {
     public int y = pacman.getY();
     public ArrayList <Position> walls;
     
-    public DrawComponents(){
+    public DrawComponents() {
         walls = new ArrayList<Position>();
         walls = pacman.getWalls();
     }
     
     //Method to draw pacman in the frame
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) { 
         drawWall(g);
         ImageIcon pacmanIcon = new ImageIcon("pacmanIcon.png");
         Image pacmanImg = pacmanIcon.getImage();
         g.drawImage(pacmanImg,x,y,30,30,null);
     }
-    public void drawWall(Graphics g){
+    public void drawWall(Graphics g) {
         
         for (int i=0;i<walls.size(); i++){
             g.setColor(Color.BLUE);
@@ -32,28 +32,28 @@ public class DrawComponents extends JComponent {
         }
     }
     //Method to move pacman to right 10 pixels
-    public void moveRight(){
+    public void moveRight() {
         x = pacman.right();
         y = y;
         repaint();
     }
 
     //Method to move pacman to left 10 pixels
-    public void moveLeft(){
+    public void moveLeft() {
         x = pacman.left();
         y= y;
         repaint();
     }
 
     //Method to move pacman to down 10 pixels
-    public void moveDown(){
+    public void moveDown() {
         y = pacman.down();
         x= x;
         repaint();
     }
 
     //Method to move pacman to up 10 pixels
-    public void moveUp(){
+    public void moveUp() {
         y = pacman.up();
         x = x;
         repaint();
