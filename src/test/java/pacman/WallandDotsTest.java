2da57360b0b07aef1532878eaea67e00b79c4044
erick <esotomercado@gmail.com>
2020-07-07 14:43:40 -0700

    changes made for indentation WallandDotsTest

--- src/test/java/pacman/WallandDotsTest.java
+++ src/test/java/pacman/WallandDotsTest.java
@@ -6,7 +6,7 @@ public class WallAndDotsTest {
 	public void testExist(){
         int x=1;
         int y=1;
-		wall wall1_1 = new wall(x,y, true);
+        wall wall1_1 = new wall(x,y, true);
         assertEquals(1,wall1_1.getY());
         assertEquals(1,wall1_1.getX());
     }
