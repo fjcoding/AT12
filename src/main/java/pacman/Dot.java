db98f271176ea802b047bf684cae1650b283f4c5
Samuel Loza Ramirez <starsaminf@gmail.com>
2020-07-07 16:26:13 -0400

    removed unnecesary file Dots.java

--- src/main/java/pacman/Dot.java
+++ src/main/java/pacman/Dot.java
@@ -1,13 +1,13 @@
-class Dot extends Position{
+class Dot extends Position {
     
     private boolean special;    //this special means that it makes pacman to eat ghost
     
-    public Dot(int x, int y, boolean exist){
+    public Dot(int x, int y, boolean exist) {
         super(x, y, exist);
         this.special = false;
     }
     
-    private boolean getKindofFood(){
+    private boolean getKindofFood() {
         return this.special;
     }
     
