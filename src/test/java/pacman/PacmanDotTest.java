92da01d8a843e1d97f33801524d6c9192cd68adb
Samuel Loza Ramirez <starsaminf@gmail.com>
2020-07-07 17:54:13 -0400

    Update PacmanDotTest.java and removed Ghost test

--- src/test/java/pacman/PacmanDotTest.java
+++ src/test/java/pacman/PacmanDotTest.java
@@ -1,31 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class PacmanDotTest{
+public class PacmanDotTest {
 	
 	@Test
-	public void testFoodIsEatable(){
+	public void testFoodIsEatable() {
 		Pacman p = new Pacman(5, 5, true);
 		Dot f = new Dot(5, 5, true);		
 		assertEquals(true, p.isEatable(f));	
 	}
 
 	@Test
-	public void testFoodDoesNotExist(){
+	public void testFoodDoesNotExist() {
 		Pacman p = new Pacman(5, 5, true);
 		Dot f = new Dot(5, 5, false);		
 		assertEquals(false, p.isEatable(f));		
 	}
 
 	@Test
-	public void testGhostIsNotEatable(){
+	public void testGhostIsNotEatable() {
 		Pacman p = new Pacman(5, 5, true);
 		Ghost   g = new Ghost(5, 5, true);	
 		assertEquals(false,p.isEatable(g));	
 	}
 
 	@Test
-	public void testGhostIsEatable(){
+	public void testGhostIsEatable() {
 		Pacman p = new Pacman(5, 5, true);
 		Ghost   g = new Ghost(5, 5, true);
 		g.changeEatable();		
