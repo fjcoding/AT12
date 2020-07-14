701b43a5dc9a764fd3c98cb85d5ba4bdaef3f482
Samuel Loza Ramirez <starsaminf@gmail.com>
2020-07-14 12:18:16 -0400

    Change obtain images by .class.getResources()

--- src/main/java/pacman/DrawComponents.java
+++ src/main/java/pacman/DrawComponents.java
@@ -36,7 +36,7 @@ public class DrawComponents extends JComponent {
         drawPacman(g);
     }
     public void drawPacman(Graphics g) {
-        ImageIcon pacmanIcon = new ImageIcon("pacman.gif");
+        ImageIcon pacmanIcon = new ImageIcon(DrawComponents.class.getResource("pacman.gif"));
         Image pacmanImg = pacmanIcon.getImage();
         g.drawImage(pacmanImg, x, y, 30, 30, null);
     }
@@ -56,7 +56,7 @@ public class DrawComponents extends JComponent {
         yG = ghost1.getY();
         xG2 = ghost2.getX();
         yG2 = ghost2.getY();
-        ImageIcon ghostIcon = new ImageIcon("ghost.jpg");
+        ImageIcon ghostIcon = new ImageIcon(DrawComponents.class.getResource("ghost.jpg"));
         Image ghostImg = ghostIcon.getImage();
         g.drawImage(ghostImg, xG, yG, 30, 30, this);
         g.drawImage(ghostImg, xG2, yG2, 30, 30, this);
--- src/main/java/pacman/PacmanDraw.java
+++ src/main/java/pacman/PacmanDraw.java
@@ -12,7 +12,7 @@ public class PacmanDraw extends JComponent {
     //Method to draw pacman in the frame
     public void paintComponent(Graphics g) {
         drawWall(g);
-        ImageIcon pacmanIcon = new ImageIcon("pacmanIcon.png");
+        ImageIcon pacmanIcon = new ImageIcon(DrawComponents.class.getResource("pacmanIcon.png"));
         Image pacmanImg = pacmanIcon.getImage();
         g.drawImage(pacmanImg, x, y, 30, 30, null);
     }
