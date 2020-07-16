e16d2e64d8e4cad6f9ed463ae64323218c654be1
JhordanSoto <JhordanSoto>
2020-07-16 15:05:17 -0700

    Check style fixed on Dot.class and Wall.class

--- src/main/java/pacman/Dot.java
+++ src/main/java/pacman/Dot.java
@@ -1,21 +1,17 @@
 class Dot extends Position {
-    
     private boolean isSpecial; //this special means that it makes pacman to eat ghost
 
-    public Dot(int x, int y, boolean exist) {
+    Dot(final int x, final int y, final boolean exist) {
         super(x, y, exist);
         this.isSpecial = false;
     }
-    
-    public boolean isSpecialDot(){
+    public boolean isSpecialDot() {
         return this.isSpecial;
     }
-
-    public void setSpecial(){
-        this.isSpecial=true;
+    public void setSpecial() {
+        this.isSpecial = true;
     }
-    
-    public boolean setExist(boolean exist) {
+    public boolean setExist(final boolean exist) {
         super.exist = exist;
         return true;
     }
